bf89395ea7c22f311ea849bbd15d5f2e77f809b4
veronicazzotali <veronica.zotali@digital.justice.gov.uk>
2018-07-04 13:57:31 +0100

    Fixes for addresses

--- Tipstaff.Tests/Integration/BaseController.cs
+++ Tipstaff.Tests/Integration/BaseController.cs
@@ -56,7 +56,7 @@ namespace Tipstaff.Tests.Integration
             _solicitorRepository = new SolicitorRepository(new DynamoAPI<Solicitor>());
 
             //Presenters
-            _addressPresenter = new AddressPresenter(_addressRepository);
+            _addressPresenter = new AddressPresenter(_addressRepository, _tipstaffRecordPresenter);
             _respondentPresenter = new RespondentPresenter(_respondentRepository);
             
             _caseReviewPresenter = new CaseReviewPresenter(_caseReviewRepository);
