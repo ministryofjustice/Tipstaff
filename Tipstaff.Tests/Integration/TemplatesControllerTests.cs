92c2e5a0b88242cd88ccc6aad5a7b27ec0479d9e
carlosMoJ <carlos.fernandez@digital.justice.gov.uk>
2018-07-09 15:14:15 +0100

    Fixed Admin-Template.Open and added integration test

--- Tipstaff.Tests/Integration/TemplatesControllerTests.cs
+++ Tipstaff.Tests/Integration/TemplatesControllerTests.cs
@@ -1,6 +1,7 @@
 ﻿using Moq;
 using NUnit.Framework;
 using System;
+using System.IO;
 using System.Text;
 using System.Web;
 using Tipstaff.Areas.Admin.Controllers;
@@ -110,6 +111,40 @@ namespace Tipstaff.Tests.Integration
             Assert.AreEqual("template name modified", template.TemplateName);
             Assert.AreNotEqual(true, template.AddresseeRequired);
             Assert.AreEqual("Warrant", template.Discriminator);
+        }
+
+        [Test]
+        public void Update_Should_Open_Template()
+        {
+            Models.TemplateEdit te = new Models.TemplateEdit();
+            te.Template.templateID = templateIndex.ToString();
+            te.Template.templateName = "template name";
+            te.Template.Discriminator = "Warrant";
+            te.Template.addresseeRequired = true;
+            Mock<HttpPostedFileBase> uploadFile = new Mock<HttpPostedFileBase>();
+            uploadFile
+            .Setup(f => f.ContentLength)
+            .Returns(10);
+
+            uploadFile
+                .Setup(f => f.FileName)
+                .Returns("testtemplate.xml");
+
+            uploadFile
+                .Setup(f => f.InputStream)
+                .Returns(new System.IO.MemoryStream(Encoding.UTF8.GetBytes("test file")));
+
+            te.uploadFile = uploadFile.Object;
+
+            _guidGenerator.Setup(x => x.GenerateTimeBasedGuid()).Returns(templateIndex);
+            var response = _sub.Create(te);
+
+            template = _templateRepository.GetTemplate(templateIndex.ToString());
+
+            System.Web.Mvc.FileStreamResult resp = (System.Web.Mvc.FileStreamResult) _sub.Open(templateIndex.ToString());
+            
+            Assert.AreEqual("application/msword", resp.ContentType);
+            Assert.AreEqual(Path.GetFileName(template.FilePath), resp.FileDownloadName);
         }
 
         [TearDown]
