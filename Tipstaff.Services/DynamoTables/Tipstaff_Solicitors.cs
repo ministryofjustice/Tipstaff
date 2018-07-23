27052f4bf82603a99d19f065f865623fec31b557
carlosMoJ <carlos.fernandez@digital.justice.gov.uk>
2018-07-23 15:09:39 +0100

    added TipstaffSolicitors dynamodb table

---
+++ Tipstaff.Services/DynamoTables/Tipstaff_Solicitors.cs
@@ -0,0 +1,18 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tipstaff.Services.DynamoTables
+{
+    [DynamoDBTable("Tipstaff_Tipstaff_Solicitors")]
+    public class Tipstaff_Solicitors:DynamoTable
+    {
+        [DynamoDBRangeKey]
+        public string TipstaffRecordID { get; set; }
+
+        public string SolicitorID { get; set; }
+    }
+}
