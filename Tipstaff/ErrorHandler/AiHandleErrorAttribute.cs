244099951cb6b95e4ba4d5986564d7fae1c2c7da
veronicazzotali <veronica.zotali@digital.justice.gov.uk>
2018-09-02 14:09:48 +0100

    Adding reports

--- Tipstaff/ErrorHandler/AiHandleErrorAttribute.cs
+++ Tipstaff/ErrorHandler/AiHandleErrorAttribute.cs
@@ -1,1 +1,8 @@
-//using System;//using System.Web.Mvc;//using Microsoft.ApplicationInsights;//namespace Tipstaff.ErrorHandler//{//    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)] //    public class AiHandleErrorAttribute : HandleErrorAttribute//    {//        public override void OnException(ExceptionContext filterContext)//        {//            if (filterContext != null && filterContext.HttpContext != null && filterContext.Exception != null)//            {//                //If customError is Off, then AI HTTPModule will report the exception//                if (filterContext.HttpContext.IsCustomErrorEnabled)//                {   //                    var ai = new TelemetryClient();//                    ai.TrackException(filterContext.Exception);//                } //            }//            base.OnException(filterContext);//        }//    }//}
+using System;using System.Web.Mvc;using TPLibrary.Logger;
+
+namespace Tipstaff.ErrorHandler{    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+    public class AiHandleErrorAttribute : HandleErrorAttribute    {        public override void OnException(System.Web.Mvc.ExceptionContext filterContext)        {            if (filterContext != null && filterContext.HttpContext != null && filterContext.Exception != null)            {
+                //If customError is Off, then AI HTTPModule will report the exception
+                if (filterContext.HttpContext.IsCustomErrorEnabled)                {
+                    var ai = new CloudWatchLogger();                    ai.LogError(filterContext.Exception, "AiHandleErrorAttribute");                }
+            }            base.OnException(filterContext);        }    }}
