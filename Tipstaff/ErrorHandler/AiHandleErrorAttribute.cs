d73b5a97f4b8b3cb37506dae1d9b54e19fa2e57b
veronicazzotali <veronica.zotali@digital.justice.gov.uk>
2018-09-03 11:49:36 +0100

    Adding Utils

--- Tipstaff/ErrorHandler/AiHandleErrorAttribute.cs
+++ Tipstaff/ErrorHandler/AiHandleErrorAttribute.cs
@@ -1,8 +1,24 @@
-using System;using System.Web.Mvc;using TPLibrary.Logger;
+using System;
+using System.Web.Mvc;
+using TPLibrary.Logger;
 
-namespace Tipstaff.ErrorHandler{    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
-    public class AiHandleErrorAttribute : HandleErrorAttribute    {        public override void OnException(System.Web.Mvc.ExceptionContext filterContext)        {            if (filterContext != null && filterContext.HttpContext != null && filterContext.Exception != null)            {
+namespace Tipstaff.ErrorHandler
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+    public class AiHandleErrorAttribute : HandleErrorAttribute
+    {
+        public override void OnException(System.Web.Mvc.ExceptionContext filterContext)
+        {
+            if (filterContext != null && filterContext.HttpContext != null && filterContext.Exception != null)
+            {
                 //If customError is Off, then AI HTTPModule will report the exception
-                if (filterContext.HttpContext.IsCustomErrorEnabled)                {
-                    var ai = new CloudWatchLogger();                    ai.LogError(filterContext.Exception, "AiHandleErrorAttribute");                }
-            }            base.OnException(filterContext);        }    }}
+                if (filterContext.HttpContext.IsCustomErrorEnabled)
+                {
+                    var ai = new CloudWatchLogger();
+                    ai.LogError(filterContext.Exception, "AiHandleErrorAttribute");
+                }
+            }
+            base.OnException(filterContext);
+        }
+    }
+}
