fc14f04e9801810035e0e31c48ad30b84ef52fc2
carlosMoJ <carlos.fernandez@digital.justice.gov.uk>
2018-09-18 12:17:05 +0200

    enable Admin-Templates (edition)

--- Tipstaff/Areas/Admin/Controllers/TemplatesController.cs
+++ Tipstaff/Areas/Admin/Controllers/TemplatesController.cs
@@ -52,7 +52,7 @@ namespace Tipstaff.Areas.Admin.Controllers
                     if (!Path.GetExtension(model.uploadFile.FileName.ToLower()).EndsWith("xml")) { throw new NotUploaded("Please select an XML file to upload"); }
                     if (model.uploadFile.ContentLength == 0) { throw new NotUploaded("The selected file appears to be empty, please select a different file and re-try"); }
                     //Upload
-                    var fileName = Path.Combine(Server.MapPath("~/uploads"), Path.GetFileName(model.uploadFile.FileName));
+                    var fileName = Path.Combine("C:\\TipstaffUploads", Path.GetFileName(model.uploadFile.FileName));
                     model.uploadFile.SaveAs(fileName); //Save to uploads folder     
                     XmlDocument document = new XmlDocument();
                     document.Load(fileName);
@@ -100,7 +100,7 @@ namespace Tipstaff.Areas.Admin.Controllers
                     if (!Path.GetExtension(model.uploadFile.FileName.ToLower()).EndsWith("xml")) { throw new NotUploaded("Please select an XML file to upload"); }
                     if (model.uploadFile.ContentLength == 0) { throw new NotUploaded("The selected file appears to be empty, please select a different file and re-try"); }
                     //Upload
-                    var fileName = Path.Combine(Server.MapPath("~/uploads"), Path.GetFileName(model.uploadFile.FileName));
+                    var fileName = Path.Combine("C:\\TipstaffUploads", Path.GetFileName(model.uploadFile.FileName));
                     model.uploadFile.SaveAs(fileName); //Save to uploads folder     
                     XmlDocument document = new XmlDocument();
                     document.Load(fileName);
