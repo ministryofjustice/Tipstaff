e8f844ef47c1f535eb57e1a63de42b9e5c60e3ce
Andrew McMahon <andrew.mcmahon@HMCTS.NET>
2022-03-22 17:21:37 +0000

    Using short date displayed format function within model

--- Tipstaff/Controllers/TemplateController.cs
+++ Tipstaff/Controllers/TemplateController.cs
@@ -486,14 +486,14 @@ namespace Tipstaff.Controllers
                 {
                     if (r.PNCID != null && r.PNCID != "")
                     {
-                        pncids += "(Respondent) " + r.PoliceDisplayName + " - " + r.PNCID + " - " + r.dateOfBirth.ToString() + "<w:br/>";
+                        pncids += "(Respondent) " + r.PoliceDisplayName + " - " + r.PNCID + " - " + r.DateofBirthDisplay + "<w:br/>";
                     }
                 }
                 foreach (Child c in ca.children)
                 {
                     if (c.PNCID != null && c.PNCID != "")
                     {
-                        pncids += "(Child) " + c.PoliceDisplayName + " - " + c.PNCID + " - " + c.dateOfBirth.ToString() + "<w:br/>";
+                        pncids += "(Child) " + c.PoliceDisplayName + " - " + c.PNCID + " - " + c.DateofBirthDisplay + "<w:br/>";
                     }
                 }
                 if (pncids == "")
