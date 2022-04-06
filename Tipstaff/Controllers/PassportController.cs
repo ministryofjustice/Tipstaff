f95fe896339e653720fc77898c38efaf506a8ce2
Andrew McMahon <andrew.mcmahon@HMCTS.NET>
2022-04-06 14:09:54 +0100

    Altering Controller

--- Tipstaff/Controllers/PassportController.cs
+++ Tipstaff/Controllers/PassportController.cs
@@ -10,7 +10,7 @@ namespace Tipstaff.Controllers
     [AuthorizeRedirect(MinimumRequiredAccessLevel = AccessLevel.User)]
     [Authorize]
     [ValidateAntiForgeryTokenOnAllPosts]
-    public class PassportController : DocumentController
+    public class PassportController : Controller
     {
         private TipstaffDB db = myDBContextHelper.CurrentContext;
 
