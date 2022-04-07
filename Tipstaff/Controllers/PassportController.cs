89b72b3c2e7b563ba09f5acf73d2ca26e90bd5d7
matthew.searle <matthew.searle@hmcts.net>
2022-04-07 09:34:02 +0100

    added PassportUploadModel, Upload Methods to Passport Controller, pointed upload page to passport controller

--- Tipstaff/Controllers/PassportController.cs
+++ Tipstaff/Controllers/PassportController.cs
@@ -20,7 +20,49 @@ namespace Tipstaff.Controllers
         {
             _logger = logger;
         }
-        
+
+        [HttpGet]
+        public ActionResult Upload(int id)
+        {
+            PassportUploadModel model = new PassportUploadModel();
+            model.tipstaffRecordID = id;
+            model.tipstaffRecord = db.TipstaffRecord.Find(id);
+            if (model.tipstaffRecord.caseStatus.sequence > 3)
+            {
+                TempData["UID"] = model.tipstaffRecord.UniqueRecordID;
+                return RedirectToAction("ClosedFile", "Error");
+            }
+
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Upload(PassportUploadModel model)
+        {
+            User user = db.GetUserByLoginName(User.Identity.Name.Split('\\').Last());
+            model.document.createdBy = user.DisplayName;
+            model.document.createdOn = DateTime.Now;
+            model.document.tipstaffRecordID = model.tipstaffRecordID;
+            if (model.uploadFile != null)
+            {
+                var stream = model.uploadFile.InputStream;
+                var buffer = new byte[stream.Length];
+                stream.Read(buffer, 0, buffer.Length);
+                model.passport.binaryFile = buffer;
+                model.passport.fileName = System.IO.Path.GetFileName(model.uploadFile.FileName);
+                model.passport.mimeType = model.uploadFile.ContentType;
+            }
+            if (ModelState.IsValid)
+            {
+                TipstaffRecord tr = db.TipstaffRecord.Find(model.tipstaffRecordID);
+                tr.Documents.Add(model.passport);
+                db.SaveChanges();
+                return RedirectToAction("Details", genericFunctions.TypeOfTipstaffRecord(tr), new { id = model.tipstaffRecordID });
+            }
+            return View(model);
+        }
+
+
+
         public PartialViewResult ListPassportsByRecord(int id, int? page)
         {
             TipstaffRecord w = db.TipstaffRecord.Find(id);
