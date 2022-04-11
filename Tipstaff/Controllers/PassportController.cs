949e5321d5a95466181a0f3811259b54d075c44b
matthew.searle <matthew.searle@hmcts.net>
2022-04-11 16:39:35 +0100

    testing new path

--- Tipstaff/Controllers/PassportController.cs
+++ Tipstaff/Controllers/PassportController.cs
@@ -97,10 +97,10 @@ namespace Tipstaff.Controllers
 
             if (ModelState.IsValid)
             {
-                TipstaffRecord tr = db.TipstaffRecord.Find(model.passport.tipstaffRecord.tipstaffRecordID);
+                TipstaffRecord tr = db.TipstaffRecord.Find(model.tipstaffRecord.tipstaffRecordID);
                 db.Entry(model.passport).State = EntityState.Modified;
                 db.SaveChanges();
-                return RedirectToAction("Details", genericFunctions.TypeOfTipstaffRecord(tr), new { id = model.passport.tipstaffRecord.tipstaffRecordID });
+                return RedirectToAction("Details", genericFunctions.TypeOfTipstaffRecord(tr), new { id = model.tipstaffRecord.tipstaffRecordID });
             }
 
             return View(model);
