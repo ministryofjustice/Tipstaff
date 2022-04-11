6aee7acb7c5c9772ea0cbef159852923c4242714
matthew.searle <matthew.searle@hmcts.net>
2022-04-11 15:30:45 +0100

    point to correct model parameter in passport controller edit

--- Tipstaff/Controllers/PassportController.cs
+++ Tipstaff/Controllers/PassportController.cs
@@ -92,7 +92,7 @@ namespace Tipstaff.Controllers
                 TipstaffRecord tr = db.TipstaffRecord.Find(model.tipstaffRecordID);
                 db.Entry(model.passport).State = EntityState.Modified;
                 db.SaveChanges();
-                return RedirectToAction("Details", genericFunctions.TypeOfTipstaffRecord(model.passport.tipstaffRecordID), new { id = model.passport.tipstaffRecordID });
+                return RedirectToAction("Details", genericFunctions.TypeOfTipstaffRecord(model.passport.tipstaffRecord.tipstaffRecordID), new { id = model.passport.tipstaffRecord.tipstaffRecordID });
             }
 
             return View(model);
