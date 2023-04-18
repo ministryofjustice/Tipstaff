aab3b6fb268e617b64558f941f498e2bdd1d2f35
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-04-18 15:26:47 +0100

    only allow TLSV1.2 and SSL3

--- Tipstaff/Global.asax.cs
+++ Tipstaff/Global.asax.cs
@@ -16,6 +16,7 @@ using Tipstaff.Infrastructure;
 using Castle.MicroKernel.Registration;
 using TPLibrary.Logger;
 using Microsoft.IdentityModel.Protocols;
+using System.Net;
 
 namespace Tipstaff
 {
@@ -190,6 +191,7 @@ namespace Tipstaff
         }
         protected void Application_Start(object sender, EventArgs e)
         {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Ssl3; // only allow TLSV1.2 and SSL3
             AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier;
             AreaRegistration.RegisterAllAreas();
 
