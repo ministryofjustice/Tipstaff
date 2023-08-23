271a7fb51d5a9318eb4a25913896074f5fa584d3
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2023-08-23 17:15:57 +0100

    add SearchQuery property to tag builder

--- Tipstaff/Helpers/helpers.cs
+++ Tipstaff/Helpers/helpers.cs
@@ -458,6 +458,12 @@ namespace Tipstaff.Helpers
                     CourtFileNumber.MergeAttribute("type", "hidden");
                     CourtFileNumber.MergeAttribute("value", temp.CourtFileNumber ?? "");
                     form.InnerHtml += CourtFileNumber.ToString();
+                    TagBuilder SearchQuery = new TagBuilder("input");
+                    SearchQuery.Attributes.Add("id", "SearchQuery");
+                    SearchQuery.Attributes.Add("name", "SearchQuery");
+                    SearchQuery.MergeAttribute("type", "hidden");
+                    SearchQuery.MergeAttribute("value", temp.SearchQuery ?? "");
+                    form.InnerHtml += SearchQuery.ToString();
                 }
                 else if (model is WarrantListViewModel)
                 {
@@ -766,6 +772,12 @@ namespace Tipstaff.Helpers
                 CourtFileNumber.MergeAttribute("type", "hidden");
                 CourtFileNumber.MergeAttribute("value", temp.CourtFileNumber ?? "");
                 form.InnerHtml += CourtFileNumber.ToString();
+                TagBuilder SearchQuery = new TagBuilder("input");
+                SearchQuery.Attributes.Add("id", "SearchQuery");
+                SearchQuery.Attributes.Add("name", "SearchQuery");
+                SearchQuery.MergeAttribute("type", "hidden");
+                SearchQuery.MergeAttribute("value", temp.SearchQuery ?? "");
+                form.InnerHtml += SearchQuery.ToString();
             }
             //Add leading fields to DIV
             form.InnerHtml += caseStatusID.ToString();
