ea30e2924dde6e0a01483bb4c7497ae1b71220c6
Timi Olubusi <timiolubusi@icloud.com>
2026-07-27 14:57:10 +0100

    Removing the templateXML field

--- Tipstaff/Models/TemplateModels.cs
+++ Tipstaff/Models/TemplateModels.cs
@@ -52,7 +52,6 @@ namespace Tipstaff.Models
         public string templateName { get; set; }
         //[Required]
         //public string templateMHT { get; set; }
-        public string templateXML { get; set; }
         public byte[] templateDOTX { get; set; }
         [Display(Name="Is addressee required?")]
         public bool addresseerequired { get; set; }
